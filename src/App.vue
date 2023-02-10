6c73980623e300af9a7b9afe97118a7918c1116e
Junior Santos <nuno.ribeiro05@hotmail.com>
2023-02-10 18:05:36 -0300

    Adicionando template e lógica sem componentização

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header class="header">
+    <div class="container_img_header">
+      <img
+        :src="require('./assets/Pokedex.png')"
+        alt="imagem pokedex"
+        class="img_header"
+      />
+    </div>
+    <h1 class="h1_title">Pokedex</h1>
+  </header>
+
+  <section class="section">
+    <label for="pokemonInput">
+      Quem é esse Pokémon?
+      <input type="text" id="pokemonInput" v-model="pokemonID" />
+    </label>
+    <button class="searchButton" @click="searchPokemon">Pesquisar</button>
+  </section>
+
+  <main class="main" v-if="Object.entries(pokemonData).length > 0">
+    <section class="pokemonCard">
+      <div class="nameImage">
+        <h1 class="pokemonName">{{ pokemonData.name }}</h1>
+        <img :src="pokemonData.sprites.front_default" :alt="pokemonData.name" />
+      </div>
+      <ul class="type">
+        <h2>Type:</h2>
+        <li
+          v-for="(type, key) in pokemonData.types"
+          :key="key"
+          :class="type.type.name"
+        >
+          <span>{{ type.type.name }}</span>
+        </li>
+      </ul>
+      <ul class="stats">
+        <h2>Stats:</h2>
+        <li v-for="(stat, key) in pokemonData.stats" :key="key">
+          <span>{{ stat.stat.name }} => {{ stat.base_stat }}</span>
+        </li>
+      </ul>
+    </section>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { baseUrl } from "./Api";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      pokemonData: {},
+      pokemonID: "",
+    };
+  },
+  methods: {
+    async searchPokemon() {
+      try {
+        const whoIsThisPokemon = await fetch(`${baseUrl}/${this.pokemonID}`);
+        const pokemon = await whoIsThisPokemon.json();
+        this.pokemonData = pokemon;
+        console.log(pokemon);
+        return pokemon;
+      } catch (error) {
+        alert("Pokemon was not found");
+      }
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
